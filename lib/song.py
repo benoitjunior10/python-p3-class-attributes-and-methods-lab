215733306c3fa087f02c7a774650771dfca4b662
benoitjunior10 <benoitjeanrobertjunior@gmail.com>
2025-09-17 19:07:43 -0400

    C'est desormais okay

--- lib/song.py
+++ lib/song.py
@@ -2,6 +2,10 @@ class Song:
     count = 0
     genre_count = {}
     artist_count = {}
+
+    genres = []
+    artists = []
+
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
@@ -11,18 +15,12 @@ class Song:
 
         if genre not in Song.genre_count:
             Song.genre_count[genre] = 0
+            Song.genres.append(genre) 
         Song.genre_count[genre] += 1
 
         if artist not in Song.artist_count:
             Song.artist_count[artist] = 0
+            Song.artists.append(artist) 
         Song.artist_count[artist] += 1
 
-    @classmethod 
-    @property
-    def genres(cls):
-        return list(cls.genre_count.keys())
     
-    @classmethod
-    @property
-    def artists(cls):
-        return list(cls.artist_count.keys())
